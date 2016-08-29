85a71891061d8fdd8b6616042989a6985db25898
Rama Milaneh <ramamilaneh@Ramas-MacBook-Air.local>
2016-08-29 17:52:07 -0400

    print

---
+++ print.playground/Contents.swift
@@ -0,0 +1,19 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var favoriteWord = " god"
+var favoriteColor = "Blue"
+var favoriteMusician = "fayroz"
+
+print(favoriteWord)
+print(favoriteColor)
+print(favoriteMusician)
+
+print("Hello")
+print("the sky is \(favoriteColor)")
+
+print(" Hi Mom. My favorite word is \(favoriteWord), my favorite color is \(favoriteColor), and my favorite Musician is \(favoriteMusician)")
+
+
+
